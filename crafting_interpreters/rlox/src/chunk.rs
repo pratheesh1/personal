826638597e446f8b2365ceb6ac311a27122d6f2d
Pratheesh Soman <pratheesh1269@gmail.com>
2023-02-21 02:20:39 +0530

    chore: match print output

--- crafting_interpreters/rlox/src/chunk.rs
+++ crafting_interpreters/rlox/src/chunk.rs
@@ -21,7 +21,7 @@ impl fmt::Display for OpCode {
     fn fmt(&self, output: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             OpCode::OpConstant(value) => {
-                let op_const = format!("{:<16} {}'{:2}' ", "OP_CONSTANT", self.code(), value);
+                let op_const = format!("{:<16} {} '{:2}' ", "OP_CONSTANT", self.code(), value);
                 write!(output, "{}", op_const.as_str())
             }
             OpCode::OpReturn => write!(output, "OP_RETURN"),
@@ -32,26 +32,28 @@ impl fmt::Display for OpCode {
 #[derive(Debug)]
 pub struct Chunk {
     code: Vec<OpCode>,
-    line: u32,
+    line: Vec<u32>,
 }
 
 impl Chunk {
     pub fn new(code: Option<OpCode>, line_num: Option<u32>) -> Self {
         let mut chunk = vec![];
+        let mut line = vec![];
+
         if let Some(code) = code {
             chunk.push(code);
+
+            if let Some(line_num) = line_num {
+                line.push(line_num);
+            }
         }
 
-        let mut line = 0;
-        if let Some(line_num) = line_num {
-            line = line_num;
-        }
         Chunk { code: chunk, line }
     }
 
     pub fn write(&mut self, byte: OpCode, line: u32) {
         self.code.push(byte);
-        self.line = line;
+        self.line.push(line);
     }
 
     pub fn disassemble(&self, name: &str)
@@ -60,17 +62,28 @@ impl Chunk {
     {
         println!("== {} ==", name);
 
+        let mut line_num: Option<&u32>;
         for (offset, instruction) in self.code.iter().enumerate() {
-            disassemble_instruction(instruction, offset, self.line);
+            if offset > 0 && (self.line.get(offset) == self.line.get(offset - 1)) {
+                line_num = None;
+            } else {
+                line_num = self.line.get(offset);
+            }
+
+            disassemble_instruction(instruction, offset, line_num);
         }
     }
 }
 
-fn disassemble_instruction<T>(instruction: &T, offset: usize, line_num: u32)
+fn disassemble_instruction<T>(instruction: &T, offset: usize, line_num: Option<&u32>)
 where
     T: std::fmt::Display,
 {
     print!("{:0width$} ", offset, width = 4);
-    print!("{:0width$} ", line_num, width = 4);
+    if let Some(line_num) = line_num {
+        print!("{:0width$} ", line_num, width = 3);
+    } else {
+        print!("  | ");
+    }
     println!("{}", instruction);
 }
--- crafting_interpreters/rlox/src/main.rs
+++ crafting_interpreters/rlox/src/main.rs
@@ -4,9 +4,9 @@ mod value;
 use chunk::{Chunk, OpCode};
 
 fn main() {
-    let mut chunk = Chunk::new(None, None);
-    chunk.write(OpCode::OpConstant(5.8), 5);
-    chunk.write(OpCode::OpReturn, 6);
+    let mut chunk = Chunk::new(None, Some(5));
+    chunk.write(OpCode::OpConstant(5.8), 123);
+    chunk.write(OpCode::OpReturn, 123);
 
     chunk.disassemble("test chunk");
 }
