6ec7e5f218e45cb4ced7bfa393c95564c4184819
Pratheesh Soman <pratheesh1269@gmail.com>
2023-06-27 22:00:58 -0600

    feat: complete chapter 21

--- crafting_interpreters/clox/debug.h
+++ crafting_interpreters/clox/debug.h
@@ -3,7 +3,7 @@
 
 #include "chunk.h"
 
-void disassembleChunk(Chunk *chunk, const char *name);
+void disassembleChunk(Chunk *chunk);
 int disassembleInstruction(Chunk *chunk, int offset);
 
 #endif // !clox_debug_h
